075d75fa0fd22597e45c06b9b3cce6fb86958862
nnson0310 <nnson0310@gmail.com>
2023-07-07 17:10:52 +0700

    Add authen flow

---
+++ ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Client.Controllers
+{
+    [Authorize]
+    public class AuthenticationController : Controller
+    {
+        public async Task Logout(HttpContext _httpContext)
+        {
+            await _httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
